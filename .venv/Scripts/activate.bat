b67ccb2f877ee9230f996877a4ccc629aec3df04
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-03-23 15:56:03 +0100

    cos

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\vdi-terminal\1sumii\.venv
+set VIRTUAL_ENV=C:\Users\vdi-terminal\1sumii\2024_L_II_NWh_INFI_K4\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
